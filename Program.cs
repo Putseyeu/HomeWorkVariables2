30cd85b7cc85fd472fe305a9832e7f275cb75872
Putseyeu <putseyeu.a.d@gmail.com>
2021-09-28 22:11:20 +0200

    step1
    step1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameShip = "merry drowned man";            
+            float totalGunpowder = 1234f;
+            byte totalOfficer = 5;
+            ushort totalKernels = 202;
+            uint totalCannons = 50;
+            long milesGoal = 350;
+            ulong minWay = 2400;
+            object literRom = 299.5;
+            int totalSailors = 90;
+            bool readyFight = totalSailors >= 40;
+        }
+    }
+}
